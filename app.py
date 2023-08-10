7ada85d2dd081a52e48e74b54a467737721acf82
boroicamarius <boroicamarius01@gmail.com>
2023-08-10 16:11:02 +0300

    restructuring code to make it easier to understand and change

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ import requests
 
 from flask import Flask, request, jsonify, make_response, Response, Request
 
+from scipy import spatial
+import numpy as np
+import math as mt
+
+from decimal import Decimal
+
+
 def _build_cors_preflight_response():
     response = make_response()
     response.headers.add("Access-Control-Allow-Origin", "*")
@@ -10,63 +17,137 @@ def _build_cors_preflight_response():
     return response
 
 
-def FindShortestPath(points,barriers):
-    # Points:  [[longitude, latitude],[...],...]
+#   TO REMEMBER:
+#       - latitude means height
+#       - longitude means width
+#
 
-    if len(points) < 2:
-        print("ERROR")
-        return {"status": 0}
+x = 0
+y = 1
 
-    query_string = ""
+longitude = x
+latitude = y
+
+
+def inverseLngLat(vector_of_coords):
+
+    resulting_vector = []
+
+    for coord in vector_of_coords:
+        #   LatLng for Leaflet.js
+        resulting_vector.append([coord[latitude], coord[longitude]])
+
+    return resulting_vector
+
+
+def transformPointsToAPIQueryString(points):
+
+    points_argument = ""
 
     for count, point in enumerate(points):
-        query_string += f"{point[0]},{point[1]}"
+        points_argument += f"{point[longitude]},{point[latitude]}"
         if count != len(points) - 1:
-            query_string += ";"
+            points_argument += ";"
 
-    query = f"https://router.project-osrm.org/route/v1/driving/{query_string}?overview=false&steps=true&geometries=geojson"
+    return points_argument
 
-    path = requests.get(query).json()["routes"][0]["legs"][0]["steps"]
+
+def makeOSMRApiCall(points):
+
+    points_argument = transformPointsToAPIQueryString(points)
+
+    query = f"https://router.project-osrm.org/route/v1/driving/{points_argument}?overview=false&steps=true&geometries=geojson"
+
+    osmr_path = requests.get(query).json()["routes"][0]["legs"][0]["steps"]
+    return osmr_path
+
+
+def extractPathFromOSMRApi(osmr_path):
+
+    path = []
+
+    for path_section in osmr_path:
+        segment_coords = path_section['geometry']['coordinates']
+        for coord in segment_coords:
+            path.append(coord)
+
+    return path
+
+
+def distanceBetween(a, b):
+    return ((b[x]-a[x])**2+(b[y]-a[y])**2)**0.5
+
+
+def distanceOfPointToLine(point, line_start, line_end):
     
-    route = []
-    osmr_path = []
+    point=np.asfarray(point)
+    line_start=np.asfarray(line_start)
+    line_end=np.asfarray(line_end)
+    
+    return np.abs(np.cross(line_end-line_start, line_start-point)) / np.linalg.norm(line_end-line_start)
 
-    for unit in path:
-        path = {}
-        path["points"] = unit["geometry"]["coordinates"]
 
-        for point in path["points"]:
-            # print(point)
-            route.append([point[1],point[0]])
+def findBarriersThatIntersectPath(path, barrier_cooordinates):
 
-        path["duration"] = unit["duration"]
-        path["distance"] = unit["distance"]
-        osmr_path.append(path)
+    unique_intersected_barriers = set()
 
-    return route
+    nearest_vector_tree = spatial.KDTree(barrier_cooordinates)
+
+    for index, coord in enumerate(path[:-1]):
+        
+        next_coord = path[index+1]
+        line_length = distanceBetween(coord, next_coord)
+        [neighbor_distance, neighbor_index] = nearest_vector_tree.query_ball_point(
+            [coord, next_coord], r=line_length)
+
+        distance_index_pair = zip(neighbor_distance, neighbor_index)
+
+        for barrier_distance, barrier_index in distance_index_pair:
+            barrier = barrier_cooordinates[barrier_index]
+
+            if distanceOfPointToLine(barrier,coord,next_coord)<0.00002:
+                unique_intersected_barriers.add(barrier_index)
+
+    return list(unique_intersected_barriers)
+
+
+def FindShortestPath(points, barrier_coordinates):
+
+    api_result = makeOSMRApiCall(points)
+
+    path = extractPathFromOSMRApi(api_result)
+
+    intersected_barriers = findBarriersThatIntersectPath(
+        path, barrier_coordinates)
+
+    return {'route': inverseLngLat(path), 'intersected_barriers': intersected_barriers}
+
 
 app = Flask(__name__)
 
-@app.route("/router", methods=["OPTIONS","POST"])
+
+@app.route("/router", methods=["OPTIONS", "POST"])
 def hello_world():
-    if request.method=="OPTIONS":
+    if request.method == "OPTIONS":
         return _build_cors_preflight_response()
     elif request.method == "POST":
         content_type = request.headers.get('Content-Type')
         data = {}
-        
+
         if (content_type == 'application/json'):
-            data = FindShortestPath(request.json['points'],request.json['barriers'])
-            
-        json = jsonify(data)    
-        
+            data = FindShortestPath(
+                request.json['points'], request.json['barriers'])
+
+        json = jsonify(data)
+
         response = make_response(json)
         response.headers.add("Access-Control-Allow-Origin", "*")
-        response.mimetype='application/json'
-        
+        response.mimetype = 'application/json'
+
         return response
     else:
-        raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
+        raise RuntimeError(
+            "Weird - don't know how to handle method {}".format(request.method))
 
 
 if __name__ == "__main__":
--- src/components/map.js
+++ src/components/map.js
@@ -1,142 +1,32 @@
 import { useEffect, useState } from "react";
 
-import L from "leaflet";
+import L, { map } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "./lf.css";
 
 import styles from "./map.module.scss";
 
-import { GrLocationPin as StartIcon } from "react-icons/gr";
-import { GrLocationPin as FlagIcon } from "react-icons/gr";
-
+import { GrLocationPin as MarkerIcon } from "react-icons/gr";
 import { renderToStaticMarkup } from "react-dom/server";
 
-async function triggerChange(map, start, end,barriers) {
-  console.log(`ROUTE FROM: ${start}->${end}`);
+const createMap = () => {
+  return L.map("map");
+};
 
-  const res = await fetch(`http://127.0.0.1:5000/router`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      points: [
-        [start.lng, start.lat],
-        [end.lng, end.lat],
-      ],
-      barriers: barriers
-    }),
+const createOSMTileLayer = () => {
+  return L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: "© OpenStreetMap",
   });
-
-  var all_coords = await res.json();
-
-  var new_polyline = new L.Polyline(all_coords, {
-    color: "red",
-    weight: 3,
-    opacity: 0.5,
-    smoothFactor: 1,
-  });
-
-  new_polyline.addTo(map);
-
-  return new_polyline;
-}
+};
 
 export default function Map(props) {
   useEffect(() => {
-    var currentPos = [51.505, -0.09];
-    var targetPos = [];
-    var map = L.map("map").setView([51.505, -0.09], 13);
+    var mapContainer = createMap().setView([51.505, -0.09], 13);
+    var tileLayer = createOSMTileLayer();
 
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
-      attribution: "© OpenStreetMap",
-    }).addTo(map);
-
-    const CarIcon_multiplier = 3;
-    const CarIcon = new L.divIcon({
-      className: styles.carIcon,
-      iconSize: [45, 60],
-      iconAnchor: [22.5, 50],
-      html: renderToStaticMarkup(<StartIcon />),
-    });
-
-    const BarrierIcon = new L.divIcon({
-      className: styles.barrierIcon,
-      iconSize: [45, 60],
-      iconAnchor: [22.5, 50],
-      html: renderToStaticMarkup(<StartIcon />),
-    });
-
-    var polyline = undefined;
-    var TargetLocationMarker = undefined;
-    var CarMarker = new L.Marker(currentPos);
-    CarMarker.setIcon(CarIcon).addTo(map);
-    CarMarker.dragging.enable();
-
-    var barrierCoords = []
-    var barriers = []
-
-    console.log("START: " + CarMarker.getLatLng());
-
-    CarMarker.on("dragend", (e) => {
-      const markerGeoPosition = e.target.getLatLng();
-      const vectorPosition = [markerGeoPosition.lat, markerGeoPosition.lng];
-      CarMarker.setLatLng(vectorPosition);
-      currentPos = vectorPosition;
-      map.setView(currentPos);
-      console.log("START: " + CarMarker.getLatLng());
-      if(polyline!=undefined) polyline.removeFrom(map);
-      if (TargetLocationMarker)
-        triggerChange(
-          map,
-          CarMarker.getLatLng(),
-          TargetLocationMarker.getLatLng(),
-          barrierCoords
-        ).then((res) => {
-          polyline = res;
-        });
-    });
-
-    const TargetIcon = new L.divIcon({
-      className: styles.flagIcon,
-      iconSize: [45, 60],
-      iconAnchor: [22.5, 50],
-      html: renderToStaticMarkup(<FlagIcon />),
-    });
-
-    map.on("click", (e) => {
-      const clickLatLng = e.latlng;
-      const vectorLatLng = [clickLatLng.lat, clickLatLng.lng];
-
-      if (TargetLocationMarker == undefined) {
-        TargetLocationMarker = new L.Marker(vectorLatLng);
-        TargetLocationMarker.setIcon(TargetIcon);
-        TargetLocationMarker.addTo(map);
-      } else TargetLocationMarker.setLatLng(vectorLatLng);
-      console.log("END: " + TargetLocationMarker.getLatLng());
-
-      while (barriers.length > 0) {barriers.pop().removeFrom(map),barrierCoords.pop()}
-
-      for(let barrier_num=1;barrier_num<=100;++barrier_num){
-
-        const barriersOffset = 0.02;
-        const modifiedLatLng = [vectorLatLng[0]+barriersOffset*Math.random()-barriersOffset/2,vectorLatLng[1]+barriersOffset*Math.random()-barriersOffset/2]
-
-        barrierCoords.push([modifiedLatLng[1],modifiedLatLng[0]])
-        barriers.push(new L.Marker(modifiedLatLng).setIcon(BarrierIcon).addTo(map))
-      }
-
-      if(polyline!=undefined) polyline.removeFrom(map);
-      triggerChange(
-        map,
-        CarMarker.getLatLng(),
-        TargetLocationMarker.getLatLng(),
-        barrierCoords
-      ).then((res) => {
-        polyline = res;
-      });
-    });
+    tileLayer.addTo(mapContainer);
   });
+
   return <div id="map" {...props} />;
 }
